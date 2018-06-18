7b82b35461f6c3e932961aefcafec146b644f342
Yipeng Sun <syp@umd.edu>
2018-06-17 22:06:31 -0400

    Bootstrap arbitrary user for X11 forwarding.

--- hep_ml/bootstrap.sh
+++ hep_ml/bootstrap.sh
@@ -3,9 +3,12 @@
 # Add a user with correct uid and gid at runtime
 mkdir -p /home/physicist
 
-echo "physicist:x:${UID}:${GID}:Physicist,,,:/home/physicist:/bin/zsh" >> \
+echo "physicist:x:${UID}:${GID}:Physicist,,,:/home/physicist:/bin/bash" >> \
     /etc/passwd
 echo "physicist:x:${UID}:" >> /etc/group
+
+# Fake a passwd entry
+echo "physicist::17640:0:99999:7:::" >> /etc/shadow
 
 chown ${UID}:${GID} -R /home/physicist
 

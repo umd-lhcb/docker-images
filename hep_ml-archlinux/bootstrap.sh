3c8bbdebdbe674f3d557582ab643182bb96f3abf
Yipeng Sun <syp@umd.edu>
2018-06-17 23:56:42 -0400

    Working on a hep_ml image that is based on arch.

---
+++ hep_ml-archlinux/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Add a user with correct uid and gid at runtime
+mkdir -p /home/physicist
+
+echo "physicist:x:${UID}:${GID}:Physicist,,,:/home/physicist:/bin/bash" >> \
+    /etc/passwd
+echo "physicist:x:${GID}:" >> /etc/group
+
+# Fake a passwd entry
+echo "physicist::17640:0:99999:7:::" >> /etc/shadow
+
+chown ${UID}:${GID} -R /home/physicist
+
+# Open to the persistent working
+cd /data
+su physicist

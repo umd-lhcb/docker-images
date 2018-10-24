e45327aaadae58f2842a2114c275aa5765c9d329
Yipeng Sun <syp@umd.edu>
2018-10-24 02:08:10 -0400

    Make shell environment 'better'.

---
+++ lhcb-stack-cc7/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Add a user with correct uid and gid at runtime
+#mkdir -p /home/physicist
+
+echo "physicist:x:${UID}:${GID}:Physicist,,,:/home/physicist:/usr/bin/zsh" >> \
+    /etc/passwd
+echo "physicist:x:${GID}:" >> /etc/group
+
+# Fake a passwd entry
+echo "physicist::17640:0:99999:7:::" >> /etc/shadow
+
+# Grant the correct permissions
+chown ${UID}:${GID} -R /home/physicist
+
+# cd to persistent working directory
+cd /data
+exec su physicist && source /opt/lhcb/LbLogin.sh

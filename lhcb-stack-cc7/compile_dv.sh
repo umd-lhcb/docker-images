a05f122053e0fe4911be6732e955c5d4a9f7fc7b
Yipeng Sun <syp@umd.edu>
2020-10-22 01:17:53 +0200

    Remove misleading comments.

--- lhcb-stack-cc7/compile_dv.sh
+++ lhcb-stack-cc7/compile_dv.sh
@@ -3,7 +3,6 @@
 DAVINCI_VERSION=v45r4
 TUPLETOOL_SL_VERSION=0.2.1
 
-# Check out TupleToolSemiLeptonic
 git config --global user.name "Physicist"
 git config --global user.email "lhcb@physics.umd.edu"
 
@@ -13,5 +12,4 @@ cd DaVinciDev_${DAVINCI_VERSION}
 git lb-use TupleToolSemiLeptonic https://github.com/umd-lhcb/TupleToolSemiLeptonic.git
 git lb-checkout TupleToolSemiLeptonic/${TUPLETOOL_SL_VERSION} Phys/TupleToolSemiLeptonic
 
-# Compile TupleToolApplyIsolation
 make configure && make
